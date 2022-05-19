0cc944a03c4aec50b8c626a93abb2a21dec03df4
Anne Saber <iarabejo3@gmail.com>
2022-05-19 18:24:26 -0500

    Included API key.

---
+++ script.js
@@ -0,0 +1,7 @@
+var searchHistory = JSON.parse(localStorage.getItem('history')) ?? []
+$(document).ready(function () {
+  var searchBtn = $('#citySearch');
+  var apiKey = 'bdf7503b42372231b8c0d6e9048c045d';
+  const prevList = document.getElementById("list")
+
+})
