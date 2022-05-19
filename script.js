68ce8aecfac3972f5ecbea9b28ff10d77c98a2eb
Anne Saber <iarabejo3@gmail.com>
2022-05-19 18:26:48 -0500

    Included search button functionality for search history of city.

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ $(document).ready(function () {
   var apiKey = 'bdf7503b42372231b8c0d6e9048c045d';
   const prevList = document.getElementById("list")
 
+
+  searchBtn.on("click", function (e) {
+    e.preventDefault()
+    var searchValue = $("#search").val()
+    searchHistory.push(searchValue)
+    localStorage.setItem("history", JSON.stringify(searchHistory))
+    $("#currentWeather").empty()
+    $("#fiveDayWeather").empty()
+    getCurrentWeather(searchValue)
+
+    searchHistory.forEach((item => {
+        let historyList = document.createElement('li');
+        historyList.textContent = `City: ${item}`;
+        prevList.appendChild(historyList)
+    }))
+})
 })
