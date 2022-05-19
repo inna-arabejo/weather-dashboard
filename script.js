8316cc4b5789eaf3ceea31f0f9167a88e10f4409
Anne Saber <iarabejo3@gmail.com>
2022-05-19 18:31:51 -0500

    Fetch request of openweather api url with latitude and longitude data.

--- script.js
+++ script.js
@@ -15,9 +15,24 @@ $(document).ready(function () {
     getCurrentWeather(searchValue)
 
     searchHistory.forEach((item => {
-        let historyList = document.createElement('li');
-        historyList.textContent = `City: ${item}`;
-        prevList.appendChild(historyList)
+      let historyList = document.createElement('li');
+      historyList.textContent = `City: ${item}`;
+      prevList.appendChild(historyList)
     }))
-})
+  })
+
+  async function getCurrentWeather(searchValue) {
+    var requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}&units=imperial`;
+    console.log('requestUrl', requestUrl)
+    let data = await (await fetch(requestUrl)).json()
+    const coords = {
+        lat: data.coord.lat,
+        lon: data.coord.lon
+    };
+
+    const fiveDayUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=imperial`
+    let data5Day = await (await fetch(fiveDayUrl)).json()
+
+    console.log("data from api", data)
+  }
 })
